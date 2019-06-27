d22d0f58475d9e304b8a8b371ccece0a6032afcc
francois nicaise <whitetigle@gmail.com>
2019-06-27 15:47:05 +0200

    added lists sets, hashes and sorted sets

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### 1.0.0-beta-002
+
+* Added definitions for lists, hashes, sets && sorted sets
+
 ### 1.0.0-beta-001
 
 * First release
--- src/Fable.Redis.fs
+++ src/Fable.Redis.fs
@@ -3,6 +3,7 @@ module rec Fable.Redis
 open System
 open Fable.Core
 open Fable
+open Fable.Core.JS
 
 let [<Import("*","redis")>] redis: Static = jsNative
 let [<Import("*","bluebird")>] bluebird: BlueBird = jsNative
@@ -23,75 +24,156 @@ type Client =
   abstract quit: unit -> unit
   
   // ===================> STRING
-  abstract APPENDAsync : key:string * value:string -> JS.Promise<int>
-  abstract BITCOUNTAsync : key:string -> JS.Promise<int>
-  abstract BITCOUNTAsync : key:string * start:int * ``end``:int -> JS.Promise<int>
+  abstract APPENDAsync : key:string * value:string -> Promise<int>
+  abstract BITCOUNTAsync : key:string -> Promise<int>
+  abstract BITCOUNTAsync : key:string * start:int * ``end``:int -> Promise<int>
   // BITFIELD
   // BITOP
-  abstract BITPOSAsync : key:string * start:int * ``end``:int -> JS.Promise<int>
-  abstract DECRAsync : key:string -> JS.Promise<int>
-  abstract DECRBYAsync : key:string * decrement:int -> JS.Promise<int>
-  abstract GETAsync : key:string -> JS.Promise<string>
-  abstract GETBITAsync : key:string * offset:int -> JS.Promise<int>
-  abstract GETRANGEAsync : key:string * start:int * ``end``:int -> JS.Promise<string>
-  abstract GETSETAsync : key:string * value:string -> JS.Promise<string>
-  abstract INCRAsync : key:string -> JS.Promise<int>
-  abstract INCRBYAsync : key:string * increment:int -> JS.Promise<int>
-  abstract INCRBYFLOATAsync : key:string * increment:float -> JS.Promise<float>
-  abstract MGETAsync : keys:string []-> JS.Promise<string []>
-  abstract MSETAsync : keys:string []-> JS.Promise<string>
-  abstract MSETNXAsync : keys:string []-> JS.Promise<string>
-  abstract PSETEXAsync : key:string * milliseconds:int * value:string -> JS.Promise<float>
-  abstract SETAsync : key:string * value:string -> JS.Promise<string>
-  abstract SETAsync : key:string * value:string * option:SetOption * option:SetOption * param:int -> JS.Promise<string>
-  abstract SETAsync : key:string * value:string * option:SetOption * param:int -> JS.Promise<string>
-  abstract SETAsync : key:string * value:string * option:SetOption -> JS.Promise<string>
-  abstract SETAsync : string [] -> JS.Promise<string>
-  abstract SETBITAsync : key:string * offset:int * value:int -> JS.Promise<int>
+  abstract BITPOSAsync : key:string * start:int * ``end``:int -> Promise<int>
+  abstract DECRAsync : key:string -> Promise<int>
+  abstract DECRBYAsync : key:string * decrement:int -> Promise<int>
+  abstract GETAsync : key:string -> Promise<string>
+  abstract GETBITAsync : key:string * offset:int -> Promise<int>
+  abstract GETRANGEAsync : key:string * start:int * ``end``:int -> Promise<string>
+  abstract GETSETAsync : key:string * value:string -> Promise<string>
+  abstract INCRAsync : key:string -> Promise<int>
+  abstract INCRBYAsync : key:string * increment:int -> Promise<int>
+  abstract INCRBYFLOATAsync : key:string * increment:float -> Promise<float>
+  abstract MGETAsync : keys:string []-> Promise<string []>
+  abstract MSETAsync : keys:string []-> Promise<string>
+  abstract MSETNXAsync : keys:string []-> Promise<string>
+  abstract PSETEXAsync : key:string * milliseconds:int * value:string -> Promise<float>
+  abstract SETAsync : key:string * value:string -> Promise<string>
+  abstract SETAsync : key:string * value:string * option:SetOption * option:SetOption * param:int -> Promise<string>
+  abstract SETAsync : key:string * value:string * option:SetOption * param:int -> Promise<string>
+  abstract SETAsync : key:string * value:string * option:SetOption -> Promise<string>
+  abstract SETAsync : string [] -> Promise<string>
+  abstract SETBITAsync : key:string * offset:int * value:int -> Promise<int>
   // SETEX : use SET + EX
   // SETNX : use SET + NX
-  abstract SETRANGEAsync : key:string * start:int * value:string -> JS.Promise<int>
-  abstract STRLENAsync : key:string -> JS.Promise<int>
+  abstract SETRANGEAsync : key:string * start:int * value:string -> Promise<int>
+  abstract STRLENAsync : key:string -> Promise<int>
   
   // ===================> KEYS
-  abstract DELAsync : key:string -> JS.Promise<int>
-  abstract DELAsync : keys:string []-> JS.Promise<int>
-  abstract DUMPAsync : key:string-> JS.Promise<string>
-  abstract EXISTSAsync : key:string -> JS.Promise<int>
-  abstract EXPIREAsync : key:string * timeout:int -> JS.Promise<int>
-  abstract EXPIREATAsync : key:string * timestamp:int -> JS.Promise<int>
-  abstract KEYSAsync : key:string -> JS.Promise<string []>
-  abstract MOVEAsync : key:string * db:int -> JS.Promise<int>
-  abstract PERSISTAsync : key:string -> JS.Promise<int>
-  abstract PEXPIREAsync : key:string * timeout:int -> JS.Promise<int>
-  abstract PEXPIREATAsync : key:string * timestamp:int -> JS.Promise<int>
-  abstract PTTLAsync : key:string -> JS.Promise<int>
-  abstract RANDOMKEYAsync : unit -> JS.Promise<string>
-  abstract RENAMEAsync : key:string * newkey:string -> JS.Promise<int>
-  abstract RENAMENXAsync : key:string * newkey:string -> JS.Promise<int>
+  abstract DELAsync : key:string -> Promise<int>
+  abstract DELAsync : keys:string []-> Promise<int>
+  abstract DUMPAsync : key:string-> Promise<string>
+  abstract EXISTSAsync : key:string -> Promise<int>
+  abstract EXPIREAsync : key:string * timeout:int -> Promise<int>
+  abstract EXPIREATAsync : key:string * timestamp:int -> Promise<int>
+  abstract KEYSAsync : key:string -> Promise<string []>
+  abstract MOVEAsync : key:string * db:int -> Promise<int>
+  abstract PERSISTAsync : key:string -> Promise<int>
+  abstract PEXPIREAsync : key:string * timeout:int -> Promise<int>
+  abstract PEXPIREATAsync : key:string * timestamp:int -> Promise<int>
+  abstract PTTLAsync : key:string -> Promise<int>
+  abstract RANDOMKEYAsync : unit -> Promise<string>
+  abstract RENAMEAsync : key:string * newkey:string -> Promise<int>
+  abstract RENAMENXAsync : key:string * newkey:string -> Promise<int>
   // RESTORE
   // SORT
   // TOUCH
-  abstract TTLAsync : key:string -> JS.Promise<int>
+  abstract TTLAsync : key:string -> Promise<int>
   // TYPE
   // UNLINK
   // WAIT
   // SCAN
 
   // ===================> LISTS
+  // BLPOP
+  // BRPOP
+  // BRPOPLPUSH
+  abstract LINDEXAsync : listName:string * index:int -> Promise<string>
+  //LINSERT
+  abstract LLENAsync : listName:string -> Promise<int>
+  abstract LPOPAsync : listName:string -> Promise<string>
+  abstract LPUSHAsync : listName:string * value:string -> Promise<int>
+  abstract LPUSHXAsync : listName:string * value:string -> Promise<int>
+  abstract LRANGEAsync : listName:string * start:int * ``end``:int -> Promise<string []>
+  abstract LREMAsync : listName:string * count:int * value:string -> Promise<int>
+  abstract LSETAsync : listName:string * index:int * value:string -> Promise<string>
+  abstract LTRIMAsync : listName:string * start:int * ``end``:int -> Promise<string>
+  abstract RPOPAsync : listName:string -> Promise<string>
+  abstract RPOPLPUSHAsync : listName:string * listName:string -> Promise<string>
+  abstract RPUSHAsync : listName:string * value:string -> Promise<int>
+  abstract RPUSHXAsync : listName:string * value:string -> Promise<int>
+
   // ===================> HASHES
+  abstract HSETAsync : hashName:string * key:string * value:string -> Promise<int>
+  abstract HSETAsync : data:string [] -> Promise<int>
+  abstract HDELAsync : hashName:string * fieldName:string -> Promise<int>
+  abstract HEXISTSAsync : hashName:string * fieldName:string -> Promise<int>
+  abstract HGETASync : hashName:string * fieldName:string -> Promise<string>
+  abstract HGETALLASync : hashName:string -> Promise<string []>
+  abstract HINCRBYASync : hashName:string * fieldName:string * increment:int -> Promise<int>
+  abstract HINCRBYFLOATASync : hashName:string * fieldName:string * increment:float -> Promise<float>
+  abstract HKEYSASync : hashName:string -> Promise<string []>
+  abstract HLENASync : hashName:string -> Promise<int>
+  abstract HMGETASync : data:string [] -> Promise<string []>
+  abstract HMSETASync : data:string [] -> Promise<string>
+  // HSCAN
+  abstract HSETNXAsync : hashName:string * key:string * value:string -> Promise<int>
+  abstract HSETNXAsync : data:string [] -> Promise<int>
+  abstract HSTRLENASync : hashName:string * fieldName:string -> Promise<int>
+  abstract HVALSASync : hashName:string -> Promise<string []>
+
   // ===================> SETS
+  abstract SADDAsync : setName:string * value:string -> Promise<int>
+  abstract SCARDAsync : setName:string -> Promise<int>
+  abstract SDIFFAsync : keys:string [] -> Promise<string []>
+  abstract SDIFFSTOREAsync : keys:string [] -> Promise<int>
+  abstract SINTERAsync : keys:string [] -> Promise<string []>
+  abstract SINTERSTOREAsync : keys:string [] -> Promise<string []>
+  abstract SISMEMBERAsync : setName:string * value:string -> Promise<int>
+  abstract SMEMBERSAsync : setName:string -> Promise<string []>
+  abstract SMOVEAsync : fromSet:string * toSet:string * value:string -> Promise<int>
+  abstract SPOPAsync : setName:string -> Promise<string>
+  abstract SRANDMEMBERAsync : setName:string -> Promise<string>
+  abstract SRANDMEMBERAsync : setName:string * index:int -> Promise<string []>
+  abstract SREMAsync : setName:string * value:string -> Promise<int>
+  // SSCAN
+  abstract SUNIONAsync : keys:string [] -> Promise<string []>
+  abstract SUNIONSTOREAsync : keys:string [] -> Promise<int>
+  
   // ===================> SORTED SETS
+  // BZPOPMAX
+  // BZPOPMIN
+  abstract ZADDAsync : setName:string * fieldName:string * value:string -> Promise<int>
+  abstract ZADDAsync : data:string [] -> Promise<int>
+  abstract ZCARDAsync : setName:string -> Promise<int>
+  abstract ZINCRBYAsync : setName:string * increment:int * value:string -> Promise<int>
+  abstract ZCOUNTAsync : setName:string * rangeFrom:int * rangeTo:int-> Promise<int>
+  abstract ZCOUNTAsync : data:string [] -> Promise<int>
+  // ZINTERSTORE
+  abstract ZLEXCOUNTAsync : data:string [] -> Promise<int>
+  abstract ZPOPMAX : setName:string -> Promise<string []>
+  abstract ZPOPMIN : setName:string -> Promise<string []>
+  abstract ZRANGEAsync : key:string * startIndex:int * endIndex:int -> Promise<string []>
+  abstract ZRANGEAsync : data:string [] -> Promise<string []>
+  // ZRANGEBYLEX
+  abstract ZRANGEBYSCOREAsync : data:string [] -> Promise<string []>
+  abstract ZRANKAsync : setName:string * fieldName:string -> Promise<int>  
+  abstract ZREMAsync : setName:string * fieldName:string -> Promise<int>  
+  // ZREMRANGEBYLEX
+  // ZREMRANGEBYRANK
+  // ZREMRANGEBYSCORE
+  // ZREVRANGE
+  // ZREVRANGEBYLEX
+  // ZREVRANGEBYSCORE
+  abstract ZREVRANKAsync : setName:string * fieldName:string -> Promise<int>  
+  // ZSCAN
+  abstract ZSCOREAsync : setName:string * fieldName:string -> Promise<string>  
+  // ZUNIONSTORE
 
 
   // ===================> CONNECTION
   // AUTH
-  abstract ECHOAsync : key:string -> JS.Promise<string>
-  abstract PINGAsync : key:string -> JS.Promise<string>
-  abstract PINGAsync : unit -> JS.Promise<string>
+  abstract ECHOAsync : key:string -> Promise<string>
+  abstract PINGAsync : key:string -> Promise<string>
+  abstract PINGAsync : unit -> Promise<string>
   // QUIT
   // SELECT
-  abstract SWAPDBAsync : db1:int * db2:int -> JS.Promise<string>
+  abstract SWAPDBAsync : db1:int * db2:int -> Promise<string>
 
   // ===================> SCRIPTING
   // ===================> PUBUB
